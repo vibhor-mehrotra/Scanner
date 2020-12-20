5679e5fe8248f7e6f31854d584321986eb734d5e
vibhor1523 <vib.meh@gmail.com>
2020-12-20 23:48:57 +0530

    Initial Commit

---
+++ Scanner/Scanner.h
@@ -0,0 +1,18 @@
+//
+//  Scanner.h
+//  Scanner
+//
+//  Created by Vibhor Mehrotra on 20/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Scanner.
+FOUNDATION_EXPORT double ScannerVersionNumber;
+
+//! Project version string for Scanner.
+FOUNDATION_EXPORT const unsigned char ScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Scanner/PublicHeader.h>
+
+
