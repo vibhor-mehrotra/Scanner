d2b57687610a9f2eac0f23cbef5f1ea86971c0a8
vibhor1523 <vib.meh@gmail.com>
2020-12-26 14:52:32 +0530

    Error message improved

--- Scanner/ScannerError.swift
+++ Scanner/ScannerError.swift
@@ -20,7 +20,7 @@ extension ScannerError: LocalizedError{
     var errorDescription: String?{
         switch self {
         case .cameraInaccessable:
-            return "Unable to access back camera!"
+            return "Unable to access back camera. Please provide camera access to the app from settings and try again."
         case let .cameraInitiationError(message):
             return "Unable to initialize back camera: \(message)"
         case .invalidImage:
